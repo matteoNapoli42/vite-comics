cd4977ecbc98638122868c56c178ac1b585b2574
matteoNapoli42 <matteon4242@gmail.com>
2023-09-22 17:31:27 +0200

    finish header structure

---
+++ src/components/Header.vue
@@ -0,0 +1,51 @@
+<script>
+</script>
+
+<template>
+    <header>
+        <div class="d-flex headerContent">
+            <div id="logoZone"><img src=".././assets/img/dc-logo.png" alt="" style="height: 86px;"></div>
+            <div id="navBarZone" class="d-flex">
+
+                <a href="">Characters</a>
+                <a href="">Comics</a>
+                <a href="">Movies</a>
+                <a href="">TV</a>
+                <a href="">Games</a>
+                <a href="">Collectibles</a>
+                <a href="">Videos</a>
+                <a href="">Fans</a>
+                <a href="">News</a>
+                <a href="">Shop</a>
+
+            </div>
+        </div>
+    </header>
+</template>
+
+<style scoped>
+header {
+    height: 118px;
+    width: 100%;
+}
+
+.headerContent {
+    width: 80%;
+    margin: auto;
+}
+
+#logoZone {
+    width: 50%;
+    padding-top: 10px;
+}
+
+#navBarZone {
+    width: 50%;
+    justify-content: space-around;
+    padding-top: 50px
+}
+
+.d-flex {
+    display: flex;
+}
+</style>
